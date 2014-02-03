a702f36a8ba046182c881975042a5668f1b45255
Szabó Gábor <szabogabi@gmail.com>
2014-02-03 14:49:57 +0100

    fb script on

--- templates/footer.php
+++ templates/footer.php
@@ -11,10 +11,10 @@
 
 <?php wp_footer(); ?>
 <div id="fb-root"></div>
-<!--script>(function(d, s, id) {
+<script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=171677096215391";
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=1389733414622982";
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script-->
+}(document, 'script', 'facebook-jssdk'));</script>
