04a77c09a561af591d67d6f2e694de43836d7107
Szabó Gábor <szabogabi@gmail.com>
2014-03-07 15:37:19 +0100

    golyo menu

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '99e21ec73b3d931a2f12896ee85006fd');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8ce1716534d09e4e97d5b6e8d86ab91c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
